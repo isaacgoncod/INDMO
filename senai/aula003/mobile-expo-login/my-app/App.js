7779fab51e5b55befccfaaa2ae9cdb370474625b
isaacgoncod <isaacgoncod@gmail.com>
2023-04-28 19:24:52 -0300

    mobile-expo-login

--- senai/aula003/mobile-expo-login/my-app/App.js
+++ senai/aula003/mobile-expo-login/my-app/App.js
@@ -1,34 +1,34 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ScreenIMC from './components/FormIMC';
+import LoginForm from './components/loginForm';
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    // Provider
     <NavigationContainer>
+
+      {/* Container de navegaçao */}
       <Stack.Navigator>
+
+        {/* Telas */}
         <Stack.Screen
-          name="login"
-          component={LoginForm}
-          options={{ title: "Bem Vindo" }}
-        />
+            name="Login"
+            component={LoginForm}
+            options={{ title: 'Bem vindo ' }} />
+            
         <Stack.Screen
-          name="Dashboard"
-          component={Dashboard}
-          options={{ title: "Dashboard" }}
+            name="telaIMC"
+            component={ScreenIMC}
+            options={{ title: 'Tela Imc' }}
         />
+
       </Stack.Navigator>
+
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
